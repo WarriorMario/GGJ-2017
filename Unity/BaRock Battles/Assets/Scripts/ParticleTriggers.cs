f21676de06cbd0af2d2bfd9754c903205de38ae4
150846 <150846@nhtv.nl>
2017-01-21 23:52:26 +0100

    [Add] Added particle prefabs, a global trigger script and a particle trigger script that will handle only particle triggers.

---
+++ Unity/BaRock Battles/Assets/Scripts/ParticleTriggers.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTriggers : MonoBehaviour {
+
+    // List of particle effects that the player has to handle
+    public GameObject m_droppingParticle;
+    public GameObject m_DrumParticle;
+    public GameObject m_fluteParticle;
+    public GameObject m_hitMarkerParticle;
+    public GameObject m_speedParticle;
+    public GameObject m_violinParticle;
+
+    public void Start()
+    {
+        SpawnDroppingParticle(FindObjectOfType<PlayerControl>());
+    }
+
+    public void SpawnVictoryParticle(PlayerControl m_victor)
+    {
+
+    }
+
+    public void SpawnDroppingParticle(PlayerControl a_player)
+    {
+        Instantiate(m_droppingParticle, a_player.transform);
+    }
+
+    public void SpawnDrumParticle(PlayerControl a_player, Wave a_wave)
+    {
+        Instantiate(m_DrumParticle, a_player.transform);
+    }
+
+    public void SpawnViolinParticle(PlayerControl a_player, Wave a_wave)
+    {
+        Instantiate(m_violinParticle, a_player.transform);
+    }
+
+    public void SpawnFluteParticle(PlayerControl a_player, Wave a_wave)
+    {
+        Instantiate(m_fluteParticle, a_player.transform);
+    }
+
+    public void SpawnDidgeridooParticle(PlayerControl a_player, PlayerControl a_target)
+    {
+
+    }
+
+    public void SpawnShieldParticle(BlockPole a_pole)
+    {
+
+    }
+
+    public void SpawnCloneParticle(PlayerControl a_player)
+    {
+
+    }
+
+    public void SpawnDodgeParticle(PlayerControl a_player)
+    {
+        Instantiate(m_speedParticle, a_player.transform);
+    }
+
+    public void SpawnSpeedParticle(PlayerControl a_player)
+    {
+        Instantiate(m_speedParticle, a_player.transform);
+    }
+}
