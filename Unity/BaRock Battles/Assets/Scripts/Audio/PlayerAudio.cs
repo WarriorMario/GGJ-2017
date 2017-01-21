7dab2e93d0ab94e5017af71080a4394bf63e6910
Raymi Klingers <WarriorMario@live.nl>
2017-01-21 22:24:55 +0100

    Adding different player prefabs Adding player sounds

---
+++ Unity/BaRock Battles/Assets/Scripts/Audio/PlayerAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField]
+    public PlayerSound[] m_NormalAttacks;
+    public PlayerSound m_SpecialAttack;
+    [System.Serializable]
+    public class PlayerSound
+    {
+        public AudioClip audioClip;
+        public float volume;
+    }
+}
