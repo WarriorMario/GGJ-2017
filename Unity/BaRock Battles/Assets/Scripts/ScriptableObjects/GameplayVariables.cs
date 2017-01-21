6eb71056ae8a68def7be12cbeedaa1a6f5d936df
Raymi Klingers <WarriorMario@live.nl>
2017-01-21 21:48:08 +0100

    Fixing merge error

--- Unity/BaRock Battles/Assets/Scripts/ScriptableObjects/GameplayVariables.cs
+++ Unity/BaRock Battles/Assets/Scripts/ScriptableObjects/GameplayVariables.cs
@@ -46,15 +46,12 @@ public class StrangeVariables : PlayerVariables
 public class GameplayVariables : ScriptableObject
 {
     [Header("General settings")]
-<<<<<<< .merge_file_a13120
     public Scheme     m_controls;
     public GameObject m_wavePrefab;
 
     [Space()]
-=======
     public Scheme m_controls;
     public string m_ArenaSceneName;
->>>>>>> .merge_file_a20276
 
     [Header("Player type specific")]
     public HeavyVariables   m_heavy;
