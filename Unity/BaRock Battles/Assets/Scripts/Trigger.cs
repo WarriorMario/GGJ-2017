58bcee2e7099dcc29b60af0628f49e42e8720788
Raymi Klingers <WarriorMario@live.nl>
2017-01-22 00:28:18 +0100

    Added audio to the trigger file Modified the player prefabs to have audio

--- Unity/BaRock Battles/Assets/Scripts/Trigger.cs
+++ Unity/BaRock Battles/Assets/Scripts/Trigger.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
     ParticleTriggers m_particleTriggers;
-    // audio
     // anim
 
     public void Init()
@@ -19,50 +18,60 @@ public class Trigger : MonoBehaviour
     public void PlayerDrumAttackWave(PlayerControl a_player, Wave a_wave)
     {
         m_particleTriggers.SpawnDrumParticle(a_player, a_wave);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
     }
 
     public void PlayerDrumAttackSecondary(PlayerControl a_player, BlockPole a_pole)
     {
         m_particleTriggers.SpawnShieldParticle(a_pole);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
     }
 
     public void PlayerViolinAttackWave(PlayerControl a_player, Wave a_wave)
     {
         m_particleTriggers.SpawnViolinParticle(a_player, a_wave);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
     }
 
     public void PlayerViolinAttackSecondary(PlayerControl a_player, Wave a_wave)
     {
         m_particleTriggers.SpawnSpeedParticle(a_player);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
     }
 
     public void PlayerFluteAttackWave(PlayerControl a_player, Wave a_wave)
     {
         m_particleTriggers.SpawnFluteParticle(a_player, a_wave);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
     }
 
     public void PlayerFluteAttackSecondary(PlayerControl a_player, PlayerControl a_clone)
     {
         m_particleTriggers.SpawnCloneParticle(a_player);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
     }
 
     public void PlayerDidgeridooAttack(PlayerControl a_player, PlayerControl a_target)
     {
         m_particleTriggers.SpawnDidgeridooParticle(a_player, a_target);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
     }
 
     public void PlayerDigeridooAttackSecondary(PlayerControl a_player)
     {
         m_particleTriggers.SpawnDodgeParticle(a_player);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
     }
 
     public void PlayerDies(PlayerControl a_player)
     {
         m_particleTriggers.SpawnDroppingParticle(a_player);
+        a_player.transform.GetComponentInChildren<PlayerAudio>().PlayDeathSound();
     }
 
     public void PlayerWins(PlayerControl a_player)
     {
         m_particleTriggers.SpawnVictoryParticle(a_player);
+        // Implement win sound
     }
 }
