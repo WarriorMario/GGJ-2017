cf7bacc0c806ddfef38a4dc0d7cb5123b8684c0d
Raymi Klingers <WarriorMario@live.nl>
2017-01-22 02:25:44 +0100

    Added first animation set to the game

--- Unity/BaRock Battles/Assets/Scripts/Trigger.cs
+++ Unity/BaRock Battles/Assets/Scripts/Trigger.cs
@@ -19,54 +19,63 @@ public class Trigger : MonoBehaviour
     {
         m_particleTriggers.SpawnDrumParticle(a_player, a_wave);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("Attack");
     }
 
     public void PlayerDrumAttackSecondary(PlayerControl a_player, BlockPole a_pole)
     {
         m_particleTriggers.SpawnShieldParticle(a_pole);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("SpecialAttack");
     }
 
     public void PlayerViolinAttackWave(PlayerControl a_player, Wave a_wave)
     {
         m_particleTriggers.SpawnViolinParticle(a_player, a_wave);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("Attack");
     }
 
     public void PlayerViolinAttackSecondary(PlayerControl a_player, Wave a_wave)
     {
         m_particleTriggers.SpawnSpeedParticle(a_player);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("SpecialAttack");
     }
 
     public void PlayerFluteAttackWave(PlayerControl a_player, Wave a_wave)
     {
         m_particleTriggers.SpawnFluteParticle(a_player, a_wave);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("Attack");
     }
 
     public void PlayerFluteAttackSecondary(PlayerControl a_player, PlayerControl a_clone)
     {
         m_particleTriggers.SpawnCloneParticle(a_player);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("SpecialAttack");
     }
 
     public void PlayerDidgeridooAttack(PlayerControl a_player, PlayerControl a_target)
     {
         m_particleTriggers.SpawnDidgeridooParticle(a_player, a_target);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlayNormalAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("Attack");
     }
 
     public void PlayerDigeridooAttackSecondary(PlayerControl a_player)
     {
         m_particleTriggers.SpawnDodgeParticle(a_player);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlaySpecialAttackSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("SpecialAttack");
     }
 
     public void PlayerDies(PlayerControl a_player)
     {
         m_particleTriggers.SpawnDroppingParticle(a_player);
         a_player.transform.GetComponentInChildren<PlayerAudio>().PlayDeathSound();
+        a_player.transform.GetComponentInChildren<Animator>().SetTrigger("Death");
     }
 
     public void PlayerWins(PlayerControl a_player)
