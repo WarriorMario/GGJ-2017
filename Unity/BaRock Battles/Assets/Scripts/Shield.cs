361d855f0a6962e949207feb5e19737e187ebf79
150206 <150206@nhtv.nl>
2017-01-22 14:06:16 +0100

    Adding shield animation

---
+++ Unity/BaRock Battles/Assets/Scripts/Shield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    int frame = 0;
+    void Update()
+    {
+        if(frame == 54)
+        {
+            return;
+        }
+        transform.GetChild(frame).gameObject.SetActive(false);
+        ++frame;
+        transform.GetChild(frame).gameObject.SetActive(true);
+    }
+}
